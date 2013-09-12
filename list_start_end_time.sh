795c868c584b4a7669411e0c61c3c2054ffcbe2e
xcubn <xcubn@xcubn.(none)>
2013-09-12 15:39:01 +0800

    first commit -- parse tool for android devicelog, event log

---
+++ list_start_end_time.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# get event log, device log first line and last ine
+# and get start, end time
+filelist=$(find $1 -mindepth 1 -maxdepth 1 -type f)
+for file in $filelist
+do
+  xxline=`sed -n 2p $file | awk '{print $1 " " $2}'`
+  xxxline=`tail -1 $file | awk '{print $1 " " $2}'`
+  echo $file $xxline $xxxline
+done
+
