b448c9e478164192233c05fe38335098798f3586
XC He <schosnablue@gmail.com>
2013-09-12 15:40:33 +0800

    2nd trial

--- list_start_end_time.sh
+++ list_start_end_time.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# 201309/12
 # get event log, device log first line and last ine
 # and get start, end time
+
 filelist=$(find $1 -mindepth 1 -maxdepth 1 -type f)
 for file in $filelist
 do
