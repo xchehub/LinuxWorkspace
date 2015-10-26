747e86ac1a8e3279163c1418bfeef3fa8cea3698
XC <schosnablue@gmail.com>
2015-10-26 14:58:05 +0800

    Update linux_cmds.sh

--- linux_cmds.sh
+++ linux_cmds.sh
@@ -1,36 +1,12 @@
+
 #1. get GTK version
 ~$ apt-cache policy libgtk2.0-0 libgtk-3-0
-libgtk2.0-0:
-  已安裝：2.24.22-1ubuntu1~saucy1
-  候選： 2.24.22-1ubuntu1~saucy1
-  版本列表：
- *** 2.24.22-1ubuntu1~saucy1 0
-        500 http://ppa.launchpad.net/gnome3-team/gnome3/ubuntu/ saucy/main amd64 Packages
-        500 http://ppa.launchpad.net/gnome3-team/gnome3-staging/ubuntu/ saucy/main amd64 Packages
-        100 /var/lib/dpkg/status
-     2.24.20-1ubuntu1 0
-        500 http://ubuntu.cs.nctu.edu.tw/ubuntu/ saucy/main amd64 Packages
-libgtk-3-0:
-  已安裝：3.10.7-0ubuntu1~saucy1
-  候選： 3.10.7-0ubuntu1~saucy1
-  版本列表：
- *** 3.10.7-0ubuntu1~saucy1 0
-        500 http://ppa.launchpad.net/gnome3-team/gnome3/ubuntu/ saucy/main amd64 Packages
-        100 /var/lib/dpkg/status
-     3.10.6-0ubuntu2~saucy1 0
-        500 http://ppa.launchpad.net/gnome3-team/gnome3-staging/ubuntu/ saucy/main amd64 Packages
-     3.8.6-0ubuntu3.1 0
-        500 http://ubuntu.cs.nctu.edu.tw/ubuntu/ saucy-updates/main amd64 Packages
-     3.8.4-0ubuntu3 0
-        500 http://ubuntu.cs.nctu.edu.tw/ubuntu/ saucy/main amd64 Packages
 
-
-#2. Redirect output to screen and file - tee
-http://ithelp.ithome.com.tw/question/10106956
+#2. Redirect output to screen and file - tee  http://ithelp.ithome.com.tw/question/10106956
 
 #3. display full path when use find command
 ~$ find "$PWD" -iname inputreader.cpp
-/xczone/workshop/regular_task/google_release_checking/2014W31_android_l_release/M8ULGenWWEK443MainS60StableMRCL369921002/frameworks/base/services/input/InputReader.cpp
+/zone/workshop/regular_task/google_release_checking/input/InputReader.cpp
 
 #4. check ubuntu version from command line
 #https://help.ubuntu.com/community/CheckingYourUbuntuVersion
@@ -57,4 +33,7 @@ http://ithelp.ithome.com.tw/question/10106956
 ~$ cat /proc/sys/kernel/random/entropy_avail
 
 #11. Single command to login to SSH and run program
-~$ ssh -t u16@10.116.64.19 'cd SST_PARSING_SERVER/foral/; java -jar autosyncpqcs_20151012CL18738.jar'
+~$ ssh -t uuuyy@10.99.99.19 'cd SST_PARSING_SERVER/foral/; java -jar automakr_20151012da18738.jar'
+
+#12. sort du -h output by size
+~$ du -h -d1 | sort -h
