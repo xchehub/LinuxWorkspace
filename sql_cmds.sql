06b87c3a6cf3a74d51128602fc17d205e6cb1f20
XC <schosnablue@gmail.com>
2016-02-23 16:25:14 +0800

    Update sql_cmds.sql

--- sql_cmds.sql
+++ sql_cmds.sql
@@ -3,3 +3,6 @@
 SELECT COLUMN_NAME, TABLE_NAME 
 FROM INFORMATION_SCHEMA.COLUMNS 
 WHERE COLUMN_NAME LIKE '%MyName%'
+
+//change the data type for a column in MySQL
+ALTER TABLE tablename MODIFY columnname INTEGER
