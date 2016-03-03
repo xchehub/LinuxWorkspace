4894bc6b093d3253c8da9702999ec2365fe0907b
XC <schosnablue@gmail.com>
2016-03-03 13:42:05 +0800

    Update sql_cmds.sql

--- sql_cmds.sql
+++ sql_cmds.sql
@@ -6,3 +6,9 @@ WHERE COLUMN_NAME LIKE '%MyName%'
 
 //change the data type for a column in MySQL
 ALTER TABLE tablename MODIFY columnname INTEGER
+
+// group data by hour
+SELECT HOUR( db_update_time ) AS h, COUNT( * ) 
+FROM  `some_table` 
+WHERE  `db_update_time` >  '2016-03-03'
+GROUP BY h
