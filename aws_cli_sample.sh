d9e39c909db63f462b016199c6860a7c74622e8b
XC <schosnablue@gmail.com>
2016-11-24 11:45:17 +0800

    update

--- aws_cli_sample.sh
+++ aws_cli_sample.sh
@@ -1,6 +1,6 @@
 #1. start instance
-
+aws ec2 start-instances --instance-ids i-0axxxxxxxxxxxxxxx
 #2. stop instance
-
+aws ec2 stop-instances --instance-ids i-0axxxxxxxxxxxxxxx
 #3. describe instance
 aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[] | [0], Placement.AvailabilityZone,InstanceType,State.Name]' --output table
