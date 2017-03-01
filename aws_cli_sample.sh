11d73e30a4ae427d78cac2496b266abe153ddd1d
XC <schosnablue@gmail.com>
2017-03-01 19:16:37 +0800

    Update aws_cli_sample.sh

--- aws_cli_sample.sh
+++ aws_cli_sample.sh
@@ -1,6 +1,10 @@
 #1. start instance
-aws ec2 start-instances --instance-ids i-0axxxxxxxxxxxxxxx
+~$ aws ec2 start-instances --instance-ids i-0axxxxxxxxxxxxxxx
 #2. stop instance
-aws ec2 stop-instances --instance-ids i-0axxxxxxxxxxxxxxx
+~$ aws ec2 stop-instances --instance-ids i-0axxxxxxxxxxxxxxx
 #3. describe instance
-aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[] | [0], Placement.AvailabilityZone,InstanceType,State.Name]' --output table
+~$ aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[] | [0], Placement.AvailabilityZone,InstanceType,State.Name]' --output table
+#4. memcahced memory usage
+# http://serverfault.com/questions/201814/memcached-memory-usage
+~$ echo "stats" | nc -w 1 <host> <port> | awk '$2 == "bytes" { print $2" "$3 }'
+
