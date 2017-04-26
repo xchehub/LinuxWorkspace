3563cf44f01adee7393f1cc1105b7191e72bbdd6
XC <schosnablue@gmail.com>
2017-04-26 17:52:49 +0800

    Create py_code_snippet.py

---
+++ py_code_snippet.py
@@ -0,0 +1,8 @@
+# List locally installed Python modules?
+# ref. http://stackoverflow.com/questions/739993/how-can-i-get-a-list-of-locally-installed-python-modules
+import pip
+installed_packages = pip.get_installed_distributions()
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+     for i in installed_packages])
+# sorted(["%s==%s" % (i.key, i.version) for i in pip.get_installed_distributions()])
+print(installed_packages_list)
